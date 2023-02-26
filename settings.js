b19d222675000e1194ff7b60bc1d8c48e66c6980
YURESH OFFICIAL <123641928+yureshofficial@users.noreply.github.com>
2023-02-26 14:06:58 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,24 +15,24 @@ global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
 global.owner = ['94719225807'] //ur owner number
-global.ownername = "ＱＵＥＥＮ-Ｘ ＢＯＴ" //ur owner name
-global.ytname = "CYBER JOHN" //ur yt chanel name
-global.socialm = "ＱＵＥＥＮ-Ｘ ＢＯＴ" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.ownername = "Yuresh Kavindu" //ur owner name
+global.ytname = "HELA CLOUD" //ur yt chanel name
+global.socialm = "X TROID" //ur github or insta name
+global.location = "Sri Lanka, Colombo, Kurunegala" //ur location
 
 //bot bomdy 
 //ඔයාලගෙ ස්කැන් කරපුහම එන කොඩ්👇 එක දාන්න
 global.session = "YXF5RWYwZ0I=" //session Id ඔයාලගෙ ස්කැන් කරපුහම එන කොඩ් එක ඔකට දාන්න
 global.ownernomer = "94719225807" //ur number
 global.premium = ['94719225807'] //ur premium number
-global.botname = 'QUEEN -X' //ur bot name
+global.botname = 'X TROID' //ur bot name
 global.linkz = "https://chat.whatsapp.com/Fd5MPEBn7NQ1aviL4V5fa7" //your theme url which will be displayed on whatsapp
 global.websitex = "https://chat.whatsapp.com/Fd5MPEBn7NQ1aviL4V5fa7" //ur website to be displayed
-global.botscript = 'https://github.com/yureshkav/Queen-X' //script link
+global.botscript = 'https://github.com/yureshofficial/X-Troid' //script link
 global.themeemoji = "🛸" //ur theme emoji
-global.packname = "ＱＵＥＥＮ-Ｘ ＢＯＴ" //ur sticker watermark packname
-global.author = "ＱＵＥＥＮ-Ｘ ＢＯＴ\n\n\nA whatsapp bot developed by\n   👸🏻ＱＵＥＥＮ-Ｘ ＢＯＴ👸🏻" //ur sticker watermark author
-global.wm = "ＱＵＥＥＮ-Ｘ ＢＯＴ." //ur watermark
+global.packname = "X TROID" //ur sticker watermark packname
+global.author = "X TROID🏻" //ur sticker watermark author
+global.wm = "X TROID" //ur watermark
 
 // Other
 global.sessionName = 'session'
