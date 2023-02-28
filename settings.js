1f5a40220339fac257f77c2ed2506eec19b1f9e2
YURESH OFFICIAL <123641928+yureshofficial@users.noreply.github.com>
2023-02-28 22:49:57 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,8 @@
-
 const fs = require('fs')
 const chalk = require('chalk')
-
 //aumto functioner
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
-
 //documents variants
 global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
 global.doc2 = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
@@ -13,33 +10,31 @@ global.doc3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.do
 global.doc4 = 'application/zip'
 global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
-
 //owmner v card
 global.owner = ['94719225807'] //ur owner number
-global.ownername = "X -TROID WH-BOT" //ur owner name
-global.ytname = "X -TROID WH-BOT" //ur yt chanel name
-global.socialm = "X -TROID WH-BOT" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.ownername = "Yuresh Kavindu" //ur owner name
+global.ytname = "HELA CLOUD" //ur yt chanel name
+global.socialm = "X TROID" //ur github or insta name
+global.location = "Sri Lanka, Colombo, Kurunegala" //ur location
 
 //bot bomdy 
 //ඔයාලගෙ ස්කැන් කරපුහම එන කොඩ්👇 එක දාන්න
+global.session = "RVJ2aFlzODk=" //session Id ඔයාලගෙ ස්කැන් කරපුහම එන කොඩ් එක ඔකට දාන්න
 global.session = "OG1hOUx4ZUg=" //session Id ඔයාලගෙ ස්කැන් කරපුහම එන කොඩ් එක ඔකට දාන්න
 global.ownernomer = "94719225807" //ur number
 global.premium = ['94719225807'] //ur premium number
-global.botname = 'X -TROID WH-BOT' //ur bot name
+global.botname = 'X TROID' //ur bot name
 global.linkz = "https://chat.whatsapp.com/Fd5MPEBn7NQ1aviL4V5fa7" //your theme url which will be displayed on whatsapp
 global.websitex = "https://chat.whatsapp.com/Fd5MPEBn7NQ1aviL4V5fa7" //ur website to be displayed
-global.botscript = 'https://github.com/yureshkav/Queen-X' //script link
+global.botscript = 'https://github.com/yureshofficial/X-Troid' //script link
 global.themeemoji = "🛸" //ur theme emoji
-global.packname = "X -TROID WH-BOT" //ur sticker watermark packname
-global.author = "X -TROID WH-BOT" //ur sticker watermark author
-global.wm = "X -TROID WH-BOT." //ur watermark
-
+global.packname = "X TROID" //ur sticker watermark packname
+global.author = "X TROID🏻" //ur sticker watermark author
+global.wm = "X TROID" //ur watermark
 // Other
 global.sessionName = 'session'
 global.prefa = ['','!','.','#','&']
 global.sp = ''
-
 //messages
 global.mess = {
     success: '*💭Here you go!*',
@@ -55,19 +50,16 @@ global.mess = {
     endLimit: '*💭Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours*',
     nsfw: '*💭The nsfw feature has not been activated, please contact the admin to activate*',
 }
-
 //media target
 global.thum = fs.readFileSync("./XtroidGallery/theme/cheemspic.jpg") //ur thumb pic
 global.log0 = fs.readFileSync("./XtroidGallery/theme/cheemspic.jpg") //ur logo pic
 global.err4r = fs.readFileSync("./XtroidGallery/theme/cheemspic.jpg") //ur error pic
-global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
-
+global.thumb = fs.readFileSync("./XtroidGallery/theme/cheemspic.jpg") //ur thumb pic
 //menu image maker
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-
 //adventure1
     global.limitawal = {
     premium: "Infinity",
@@ -82,12 +74,10 @@ global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutp
    umpanawal: 5,
    potionawal: 1
 }
-
 global.limitAwal = {
  prem: 'Unlimited',
  free: 70
 }
-
 //adventure2
 global.emot = {
 role: '🏆',
@@ -122,7 +112,6 @@ busur: '🏹',
 panah: '💘',
 kapak: '🪓'
 }
-
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
 	fs.unwatchFile(file)
