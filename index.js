da6db131d9c874decfc579b6605449e2994733d3
YURESH OFFICIAL <123641928+yureshofficial@users.noreply.github.com>
2023-03-02 11:14:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -166,7 +166,7 @@ XeonLft = await getBuffer(ppuser)
 	            const xdate = moment.tz('Asia/Kolkata').format('DD/MM/YYYY')
 	            const xmembers = metadata.participants.length
                 let unicorndoc = {key: {fromMe: false,"participant":"0@s.whatsapp.net", "remoteJid": "916909137213-1604595598@g.us"}, "message": {orderMessage: {itemCount: 9999999,status: 200, thumbnail: XeonWlcm, surface: 200, message: `${metadata.subject}`, orderTitle: 'xeon', sellerJid: '0@s.whatsapp.net'}}, contextInfo: {"forwardingScore":999,"isForwarded":true},sendEphemeral: true}
-                xeonbody =  `\`\`\````
+                xeonbody =  `\`\`\
 Welcome: @${xeonName.split("@")[0]}
 
 
